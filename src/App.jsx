8fded50ef6392951e2aebcb0c3ec6a4c0c642dcc
AntonNageh <92064286+AntonNageh@users.noreply.github.com>
2023-06-15 23:24:06 +0300

    FirstCommit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Footer from './Maincomponents/Footer/Footer'
+import Header from './Maincomponents/Header/Header'
+import Mainpage from './Pages/Mainpage/Mainpage'
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <Mainpage/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
